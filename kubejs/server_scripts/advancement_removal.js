409f164b134a89490e85bae883b4bc2b6bedfb57
Verbina29 <verbina29@gmail.com>
2024-08-19 01:25:13 -0500

    basic kubejs scripts

---
+++ kubejs/server_scripts/advancement_removal.js
@@ -0,0 +1,30 @@
+function removeAdvancement(advancementFilePath) {
+    let arr = []
+    if (typeof (advancementFilePath) == 'string') arr = [advancementFilePath]
+    else if (typeof (advancementFilePath) == 'object') arr = advancementFilePath
+    else console.log(`Invalid type for removeAdvancement(${advancementFilePath})`)
+
+    ServerEvents.highPriorityData(event => {
+        event.addJson('yourmodpack:advancements/removed', {
+            display: { hidden: true },
+        })
+        arr.forEach(advancement => {
+            event.addJson(`${advancement}.json`, {
+                parent: 'yourmodpack:advancements/removed',
+                display: { hidden: true },
+                criteria: {
+                    impossible: {
+                        trigger: 'minecraft:impossible'
+                    }
+                },
+                requirements: [['impossible']]
+            })
+        })
+    })
+}
+
+removeAdvancement([
+    'irons_spellbooks:grant_patchouli',
+    'hexcasting:advancements/grant_patchi_book',
+])
+
---
+++ kubejs/server_scripts/starting_items.js
@@ -0,0 +1,9 @@
+PlayerEvents.loggedIn(e=>{
+    if (!e.player.stages.has('starting_items')) {
+        e.player.stages.add('starting_items')
+        e.server.scheduleInTicks(50, callback => {
+            e.player.give(Item.of('akashictome:tome', '{"akashictome:data":{alexscaves:{Count:1b,id:"alexscaves:cave_book"},alexsmobs:{Count:1b,id:"alexsmobs:animal_dictionary"},bloodmagic:{Count:1b,id:"patchouli:guide_book",tag:{"patchouli:book":"bloodmagic:guide"}},botania:{Count:1b,id:"botania:lexicon"},embers:{Count:1b,id:"embers:ancient_codex"},goety:{Count:1b,id:"patchouli:guide_book",tag:{"patchouli:book":"goety:black_book"}},goety_0:{Count:1b,id:"patchouli:guide_book",tag:{"akashictome:definedMod":"goety_0","patchouli:book":"goety:witches_brew"}},hexcasting:{Count:1b,id:"patchouli:guide_book",tag:{"patchouli:book":"hexcasting:thehexbook"}},irons_spellbooks:{Count:1b,id:"patchouli:guide_book",tag:{"patchouli:book":"irons_spellbooks:iss_guide_book"}},mahoutsukai:{Count:1b,id:"mahoutsukai:guidebook"},naturesaura:{Count:1b,id:"patchouli:guide_book",tag:{"patchouli:book":"naturesaura:book"}},occultism:{Count:1b,id:"occultism:dictionary_of_spirits",tag:{"modonomicon:book_id":"occultism:dictionary_of_spirits"}},totemic:{Count:1b,id:"patchouli:guide_book",tag:{"patchouli:book":"totemic:totempedia"}},vampirism:{Count:1b,id:"guideapi_vp:vampirism-guidebook"},wizards_reborn:{Count:1b,id:"wizards_reborn:arcanemicon"}}}'))
+        });
+    }
+})
+
